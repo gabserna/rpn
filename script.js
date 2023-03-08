f3cb80cf5f706da6fc94ebe1d5db2050fe889d79
Gabriel Serna <gabriel.sernapoot6740@stu.mtec.edu>
2023-03-07 23:56:50 -0700

    script from rpn added

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-function rpn() {
-    
+function rpn(expression) {
+  class RPN {
+    constructor(expression) {
+    this.data = 0;
+    this.stack = [];
+    this.operators = ["+", "-", "*", "/"];
+    this.expression = expression.split(" ");
+    }
+
+    push(value) {
+    this.stack[this.data] = value;
+    this.data++;
+    }
+
+    pop() {
+    if (this.data === 0) {
+      return undefined;
+    }
+    this.data--;
+    const result = this.stack[this.data];
+    delete this.stack[this.data];
+    return result;
+    }
+
+    peek() {
+    return this.stack[this.data - 1];
+    }
+
+    size() {
+    return this.data;
+    }
+
+    evaluate() {
+    const operators = {
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      "*": (a, b) => a * b,
+      "/": (a, b) => a / b
+    };
+
+    const tokens = this.expression;
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+      if (this.operators.includes(token)) {
+        const a = this.pop();   // el orden de los factores si altera el producto
+        const b = this.pop();
+        const result = operators[token](a, b);
+        this.push(result); //intercambio
+      } else {
+        this.push(parseFloat(token));
+      }
+    }
+    return this.pop();
+    }
+  }
+
+  const theRpn = new RPN(expression);
+  return theRpn.evaluate();
 }
